38715171c13f094646499efbdd0996114ee95a68
Serhii Shynkarenko <serhii.shynkarenko@videoslots.com>
2022-04-26 20:24:51 +0300

    fix errors and reformat

--- src/Weight.php
+++ src/Weight.php
@@ -4,13 +4,16 @@ namespace Sergeshin\WeightConversion;
 
 class Weight
 {
+    private float $kilograms;
+
     public static function kilograms(float $kilograms): self
     {
         return new static($kilograms);
     }
 
-    public function __construct(protected float $kilograms)
+    public function __construct(float $kilograms)
     {
+        $this->kilograms = $kilograms;
     }
 
     public function toPounds(): float
